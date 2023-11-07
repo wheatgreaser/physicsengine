e91372792848b8d1699ebdc9dc9e06d5ef2ccd7f
wheatgreaser <108815657+wheatgreaser@users.noreply.github.com>
2023-11-07 00:58:35 +0000

    force simulation?

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ class Particle:
     self.mass = mass
     self.position = position
 
-p1 = Particle(200, [4, 5, 0])
-p2 = Particle(200, [5, 0, 3])
+p1 = Particle(200, [4, 5, 1])
+p2 = Particle(2, [5, 0, 0])
 
 def distance(p1, p2):
   l = pow((p1.position[0] - p2.position[0]), 2)
@@ -23,10 +23,7 @@ def gravitational_force(p1, p2):
   F=[0, 0, 0]
   F[0] = ((constants[0][1]* p1.mass * p2.mass* (p1.position[0] - p2.position[0])))/ pow(distance(p1, p2) , 3)
   F[1] = ((constants[0][1]* p1.mass * p2.mass*(p1.position[1] - p2.position[1])))/ pow(distance(p1, p2), 3)
-  F[2] = ((constants[0][1]* p1.mass * p2.mass *(p1.position[2] - p2.position[2])))/ pow(distance(p1, p2), 3)
-
-  
-  
+  F[2] = ((constants[0][1]* p1.mass * p2.mass *(p1.position[2] - p2.position[2])))/ pow(distance(p1, p2), 3) 
   return F 
 
 def posUpdate(v, p, t):
@@ -77,9 +74,26 @@ def simul_gravity(duration):
     print(f'distance: {distance(p1, p2)}')
     print()
     time.sleep(1)
+    
+def simul_force(force, p, duration):
+  v = [0, 0, 0]
+  for i in range(duration):    
+    a =[force[0]/p.mass,
+        force[1]/p.mass, 
+        force[2]/p.mass]
+    
+    v[0] = v[0] + a[0] 
+    v[1] += a[1]
+    v[2] += a[2]
+    print(f'X {p.position[0]}')
+    print(f'Y {p.position[1]}')
+    print(f'Z {p.position[2]}')
+    posUpdate(v, p, 1)
+  
+F = [5, 0, 0]   
+simul_force(F, p2, 10)
 
 
-simul_gravity(20)
   
   
   
